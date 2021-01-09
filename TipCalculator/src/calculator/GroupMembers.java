1088e7996353e10ac01d5c172d611628ff4c898e
Mai Nguyen <49288117+mnnguyen2@users.noreply.github.com>
2021-01-09 15:36:17 -0500

    Update GroupMembers.java

--- TipCalculator/src/calculator/GroupMembers.java
+++ TipCalculator/src/calculator/GroupMembers.java
@@ -1,30 +1,83 @@
 package calculator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 /**
- * represents the folk(s) ordering together
+ * Calculate tips and bill amount, customizing based on users' input 
+ * (tax rates, number of people to split the bill with, and tip percentage)
  * @author frank, Mai and Betty
- *
  */
-public class GroupMembers {
+public class CalculateTip {
+	
 	/**
-	 * represent the name of one of the group members
+	 * main method
+	 * @param args
 	 */
-	public String name;
-	/**
-	 * represent the amount charged to a single group member
-	 */
-	public double amount;
-	/**
-	 * represent the amount of tip that each group member is adding to their bill if they are tipping individually
-	 */
-	public double tipAmount;
-	/**
-	 * create each instance of a group member
-	 * @param name
-	 * @param amount
-	 */
-	public GroupMembers(String name, double amount) {
-		this.name = name;
-		this.amount = amount;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the Master Tip Calculator!");
+		
+		// 1. Get the total sum of the bill, not including taxes
+		System.out.println("How much is your total bill, not including tax?");
+		Scanner userInput = new Scanner(System.in);
+		double totalSum = userInput.nextDouble();
+		
+		// 2. Get the tax rate 
+		System.out.println("What is the tax percentage of your bill? (i.e 6.25)");
+		double taxRate = userInput.nextDouble();
+		
+		// 3. Get the number of people to split bill with (inclusing user)
+		System.out.println("How many people would you like to split this bill with? (including yourself)");
+		double numPeople = userInput.nextInt();
+		
+		// 4. Get the tip percentage
+		System.out.println("What is the tip percentage you'd like to put in? (i.e 20)");
+		double tipPercentage = userInput.nextDouble();
+		
+		// calculate the tip amount
+		double tipAmount = totalSum * (1 + taxRate / 100) * tipPercentage/100;
+		System.out.println("Your tip amount will be " + tipAmount);
+		// calculate the bill total after tip & tax
+		double totalBill = totalSum * (1 + taxRate /100) + tipAmount;
+		System.out.println("Your total bill amount including tax and tip will be " + totalBill);
+		
+		// 5. Ask if the user would like to split the bill evenly
+		System.out.println("Would you like to split the check evenly? Y for Yes, N for No.");
+		String evenString = userInput.nextLine();
+		
+		if (evenString.toLowerCase().startsWith("y")) {
+			double payAmount = totalBill / numPeople;
+			System.out.println("The total bill amount for each person in your party of " + numPeople + " comes to " + payAmount);
+		}
+		
+		else if (evenString.toLowerCase().startsWith("n")) {
+			String[] groupArr = new String[(int) numPeople];
+			TreeMap<String, Double> groupMap = new TreeMap<String, Double>();
+			for (int i = 0; i < groupArr.length; i++) {
+				System.out.println("Name of person #" + (i+1) + ": ");
+				groupArr[i] = userInput.nextLine(); 
+			}
+			
+			for (String name: groupArr) {
+				System.out.println("Please enter bill amount for " + name + " is: ");
+				groupMap.put(name, userInput.nextDouble());
+			}
+			
+			for (String name : groupMap.keySet()) {
+				
+				double indBillAmount = groupMap.get(name);
+				double payAmount = indBillAmount + (tipAmount + taxRate * totalSum / 100) / numPeople;
+				groupMap.put(name, payAmount);
+				System.out.println("The total bill amount for " + name + " is " + payAmount);
+			}
+
+		}
+		
+		
 	}
 
 }
