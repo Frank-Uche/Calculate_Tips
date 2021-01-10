a468303a72fa952f063c5b456407cb38fc7c292f
Mai Nguyen <49288117+mnnguyen2@users.noreply.github.com>
2021-01-10 09:10:45 -0500

    Update CalculateTip.java

--- TipCalculator/src/calculator/CalculateTip.java
+++ TipCalculator/src/calculator/CalculateTip.java
@@ -9,7 +9,7 @@ import java.util.TreeMap;
 /**
  * Calculate tips and bill amount, customizing based on users' input 
  * (tax rates, number of people to split the bill with, and tip percentage)
- * @author frank, Mai and Betty
+ * @author Franklyn, Mai and Betty
  */
 public class CalculateTip {
 	
@@ -17,7 +17,6 @@ public class CalculateTip {
 	 * main method
 	 * @param args
 	 */
-	//hello	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to the Master Tip Calculator!");
@@ -33,7 +32,7 @@ public class CalculateTip {
 		
 		// 3. Get the number of people to split bill with (including user)
 		System.out.println("How many people would you like to split this bill with? (including yourself)");
-		double numPeople = userInput.nextInt();
+		int numPeople = userInput.nextInt();
 		
 		// 4. Get the tip percentage
 		System.out.println("What is the tip percentage you'd like to put in? (i.e 20)");
@@ -50,12 +49,14 @@ public class CalculateTip {
 		System.out.println("Would you like to split the check evenly? Y for Yes, N for No.");
 		String evenString = userInput.next();
 		
+		// if the user says yes
 		if (evenString.equalsIgnoreCase("Y")) {
 			
 			double payAmount = Math.round(totalBill / numPeople);
-			System.out.println("The total bill amount for each person in your party of " + numPeople + " comes to " + payAmount);
+			System.out.println("The total bill amount for each person in your party of " + numPeople + " comes to " + payAmount + " dollars");
 		}
 		
+		// if the user says no
 		else if (evenString.equalsIgnoreCase("N")) {
 			String[] groupArr = new String[(int) numPeople];
 			TreeMap<String, Double> groupMap = new TreeMap<String, Double>();
@@ -70,9 +71,28 @@ public class CalculateTip {
 				System.out.println("Name of person #" + (i+1) + ": " + groupArr[i]);
 			}
 			
+			double checkSum = 0.0;
 			for (String name: groupArr) {
-				System.out.println("Please enter bill amount for " + name + " is: ");
+				System.out.println("Please enter bill amount for " + name + ": ");
 				groupMap.put(name, userInput.nextDouble());
+				checkSum += groupMap.get(name);
+				double remainSum = totalSum - checkSum;
+				// to help user keep track of individual bill amounts
+				System.out.println("The remaining amount in your total bill is " + remainSum);
+			}
+			
+			// check that the user has put in the correct amount for all group members
+			if (checkSum != totalSum) {
+				System.out.println("Your total bill amount does not add up correctly. Please input the correct amount for individual users. ");
+				checkSum = 0.0;
+				for (String name: groupArr) {
+					System.out.println("Please enter bill amount for " + name + ": ");
+					groupMap.put(name, userInput.nextDouble());
+					checkSum += groupMap.get(name);
+					double remainSum = totalSum - checkSum;
+					// to help user keep track of individual bill amounts
+					System.out.println("The remaining amount in your total bill is " + remainSum);
+				}
 			}
 			
 			for (String name : groupMap.keySet()) {
@@ -81,14 +101,12 @@ public class CalculateTip {
 				double payAmount = indBillAmount + (tipAmount + taxRate * totalSum / 100) / numPeople;
 				double finalTip = tipAmount * (indBillAmount / totalSum);
 				groupMap.put(name, payAmount);
-				System.out.println("The total bill amount for " + name + " is " + Math.round(payAmount));
-				System.out.println("Your tip amount is " + finalTip);
+				System.out.println("The total bill amount for " + name + " is " + Math.round(payAmount) + " dollars");
+				System.out.println("The tip amount for " + name + " is " + finalTip + " dollars");
+				System.out.println(""); // blank line
 			}
-
 		}
-		
-		
-		
+	userInput.close();
 	}
 
 }
